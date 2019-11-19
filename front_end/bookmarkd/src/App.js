527abf36257bc25b81c8f309f6fefe41a1f808a4
Paul Raucci <paulraucci@Pauls-MacBook-Pro-4.local>
2019-11-19 16:30:47 -0500

    once before lupe leaves

--- front_end/bookmarkd/src/App.js
+++ front_end/bookmarkd/src/App.js
@@ -3,6 +3,12 @@ import NewForm from "./components/NewForm";
 import axios from "axios";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookmark: []
+    };
+  }
   render() {
     return (
       <div className="App">
