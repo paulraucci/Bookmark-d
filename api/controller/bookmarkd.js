89065a4bd17f786e0e3b97c0e9d0807153c6bad7
Paul Raucci <paulraucci@Pauls-MacBook-Pro-4.local>
2019-11-19 09:25:18 -0500

    maybe fixed

--- api/controller/bookmarkd.js
+++ api/controller/bookmarkd.js
@@ -1,8 +1,29 @@
 const express = require("express");
 const bookmarkd = express.Router();
+// const Bookmarkd = Schema("../");
 
+//Index GET route
 bookmarkd.get("/", (req, res) => {
+  Schema.find({}, (error, foundBookmark) => {
+    if (error) {
+      res.send(400).json({ error: error.message });
+    } else {
+      res.status(200).json();
+    }
+  });
   res.send("index");
+});
+
+//
+//Post Route
+bookmarkd.post("/", (req, res) => {
+  Bookmarkd.create(req.body, (error, createdBookmark) => {
+    if (error) {
+      res.status(400).json({ error: error.message });
+    } else {
+      res.status(200).send(createdBookmark);
+    }
+  });
 });
 
 module.exports = bookmarkd;
